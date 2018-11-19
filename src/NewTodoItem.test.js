503e0efdfa78ceb7f6dae854fcbdee0186a8278b
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-11-20 00:16:09 +0100

    [test] move repetitive code to function

--- src/NewTodoItem.test.js
+++ src/NewTodoItem.test.js
@@ -8,6 +8,17 @@ import NewTodoItem from './NewTodoItem'
 // configure enzyme, can that be done somewhere more globally?
 configure({ adapter: new Adapter() })
 
+/**
+ * return tuple containing
+ * -   shallow wrapper around rendered component
+ * -   `onSubmit` spy function
+ */
+function getShallowWrapper () {
+  const onSubmit = getMock()
+  const component = shallow(<NewTodoItem onSubmit={onSubmit} />)
+  return [component, onSubmit]
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<NewTodoItem />, div)
@@ -15,37 +26,32 @@ it('renders without crashing', () => {
 })
 
 it('does not call `onSubmit` callback without cause', () => {
-  const spy = getMock()
-  shallow(<NewTodoItem onSubmit={spy} />)
+  const [_, spy] = getShallowWrapper()
   expect(spy.mock.calls.length).toBe(0)
 })
 
 it('does not call `onSubmit` callback on empty state', () => {
-  const spy = getMock()
-  const component = shallow(<NewTodoItem onSubmit={spy} />)
+  const [component, spy] = getShallowWrapper()
   component.find('form').simulate('submit', mockedEvent)
   expect(spy.mock.calls.length).toBe(0)
 })
 
 it('calls given `onSubmit` callback on form submit', () => {
-  const spy = getMock()
-  const component = shallow(<NewTodoItem onSubmit={spy} />)
+  const [component, spy] = getShallowWrapper()
   component.state().value = testString
   component.find('form').simulate('submit', mockedEvent)
   expect(spy.mock.calls.length).toBe(1)
 })
 
 it('calls given `onSubmit` callback on button click', () => {
-  const spy = getMock()
-  const component = shallow(<NewTodoItem onSubmit={spy} />)
+  const [component, spy] = getShallowWrapper()
   component.state().value = testString
   component.find('.icon').simulate('click', mockedEvent)
   expect(spy.mock.calls.length).toBe(1)
 })
 
 it('passes state to `onSubmit` callback', () => {
-  const spy = getMock()
-  const component = shallow(<NewTodoItem onSubmit={spy} />)
+  const [component, spy] = getShallowWrapper()
   component.state().value = testString
   component.find('form').simulate('submit', mockedEvent)
   expect(spy.mock.calls[0][0]).toBe(testString)
--- src/TodoItem.test.js
+++ src/TodoItem.test.js
@@ -8,6 +8,19 @@ import TodoItem from './TodoItem'
 // configure enzyme, can that be done somewhere more globally?
 configure({ adapter: new Adapter() })
 
+/**
+ * return tuple containing
+ * -   shallow wrapper around rendered component
+ * -   `onToggleDone` spy function
+ * -   `onDelete` spy function
+ */
+function getShallowWrapper () {
+  const onToggleDone = getMock()
+  const onDelete = getMock()
+  const component = shallow(<TodoItem model={taskItem} onToggleDone={onToggleDone} onDelete={onDelete} />)
+  return [component, onToggleDone, onDelete]
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<TodoItem model={taskItem} />, div)
@@ -15,12 +28,12 @@ it('renders without crashing', () => {
 })
 
 it('renders description of given model', () => {
-  const component = shallow(<TodoItem model={taskItem} />)
+  const [component] = getShallowWrapper()
   expect(component.text()).toMatch(new RegExp(taskItem.description + '.*'))
 })
 
 it('renders done status of given model', () => {
-  const component = shallow(<TodoItem model={taskItem} />)
+  const [component] = getShallowWrapper()
   let checkbox = component.find('input')
   expect(checkbox.props().checked).toBe(false)
   component.setProps({model: taskItemDone})
@@ -29,24 +42,20 @@ it('renders done status of given model', () => {
 })
 
 it('does not call callbacks without cause', () => {
-  const onToggleDone = getMock()
-  const onDelete = getMock()
-  shallow(<TodoItem model={taskItem} onToggleDone={onToggleDone} onDelete={onDelete} />)
+  const [_, onToggleDone, onDelete] = getShallowWrapper()
   expect(onToggleDone.mock.calls.length).toBe(0)
   expect(onDelete.mock.calls.length).toBe(0)
 })
 
 it('does call `onToggleDone` callback on button click', () => {
-  const onToggleDone = getMock()
-  const component = shallow(<TodoItem model={taskItem} onToggleDone={onToggleDone} />)
+  const [component, onToggleDone] = getShallowWrapper()
   component.find('input').simulate('change')
   expect(onToggleDone.mock.calls.length).toBe(1)
   expect(onToggleDone.mock.calls[0][0]).toBe(taskItem)
 })
 
 it('does call `onDelete` callback on button click', () => {
-  const onDelete = getMock()
-  const component = shallow(<TodoItem model={taskItem} onDelete={onDelete} />)
+  const [component, _, onDelete] = getShallowWrapper()
   component.find('.icon').simulate('click')
   expect(onDelete.mock.calls.length).toBe(1)
   expect(onDelete.mock.calls[0][0]).toBe(taskItem)
