03ac7fbacf3829941d53371e0ee7e4dbad8aaf60
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-11-19 12:31:47 +0100

    [design] add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.scss';
 import TodoList from './TodoList';
 import * as serviceWorker from './serviceWorker';
