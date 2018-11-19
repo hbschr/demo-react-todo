6589ddde1d6ab04c99a49a8510eaa379a12602d0
Heiko Hübscher <heiko.huebscher@gmail.com>
2018-11-20 00:16:42 +0100

    [wip] first attempt at integration test

--- src/TodoList.test.js
+++ src/TodoList.test.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { getMock, mockedEvent, testString } from './testUtils'
 import TodoList from './TodoList'
+
+// configure enzyme, can that be done somewhere more globally?
+configure({ adapter: new Adapter() })
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<TodoList />, div)
   ReactDOM.unmountComponentAtNode(div)
+})
+
+it('initializes with empty list', () => {
+  const component = shallow(<TodoList />)
+  expect(component.state('list')).toEqual([])
+})
+
+it('adds new item', () => {
+  const component = shallow(<TodoList />)
+  const newTodo = component.find('NewTodo')
+  // newTodo.state().value = testString
+  // component.handleCreate('lala')
+  // expect(component.state('list')).toEqual([])
 })
